96ff075a1edfe679ec2d2515ad5ea867c09805bd
Ty <poehnell28@gmail.com>
2020-10-03 11:39:49 -0600

    repair blowout

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-package Main;
-
-import Player.Job;
+import Main.GameSetup;
 
 
 public class Main {
--- src/Main/GameBoard.java
+++ src/Main/GameBoard.java
@@ -1,29 +1,65 @@
 package Main;
 
+import java.io.IOException;
+
 public class GameBoard {
-    public GameBoard() {
-        int[][] gameBoard = new int[16][31];
+    String enemyName, playerName;
+    int enemyHP, enemyMP, playerHP, playerMP;
+
+    public GameBoard(String enemyName, int enemyHP, int enemyMP, String playerName, int playerHP, int playerMP) {
+        this.enemyName = enemyName;
+        this.enemyHP = enemyHP;
+        this.enemyMP = enemyMP;
+        this.playerName = playerName;
+        this.playerHP = playerHP;
+        this.playerMP = playerMP;
+
+        int[][] gameBoard = new int[16][61];
         for (int i = 0; i < gameBoard.length; i++) {
             System.out.print('|');
             for (int k = 0; k < gameBoard[i].length; k++) {
-                if (i == 0 || i == gameBoard.length - 1) {
+                if (i == 0 && k < gameBoard[i].length / 2 || i == gameBoard.length - 1 && k < gameBoard[i].length / 2) {
                     System.out.print('-');
-                } else if (i == 1 && k == gameBoard[i].length / 2) {
+                    //enemy section of game board------------------------------------------
+                } else if (i == 1 && k == gameBoard[i].length / 4) {
                     System.out.print('E');
+                } else if (i == 1 && k == gameBoard[i].length / 2 + 6) {
+                    System.out.print(enemyName);
+                } else if (i == 2 && k == gameBoard[i].length / 2 + 2) {
+                    System.out.print("HP : " + enemyHP);
+                } else if (i == 3 && k == gameBoard[i].length / 2 + 2) {
+                    System.out.print("MP : " + enemyMP);
                 } else if (i == gameBoard.length / 2 && k >= 0 && k < gameBoard[i].length) {
                     System.out.print('`');
+//hero section of game board---------------------------------------------
+                } else if (i == gameBoard.length / 2 + 1 && k == gameBoard[i].length / 2 + 6) {
+                    System.out.print(playerName);
+                } else if (i == gameBoard.length / 2 + 2 && k == gameBoard[i].length / 2 + 2) {
+                    System.out.print("HP : " + playerHP);
+                } else if (i == gameBoard.length / 2 + 3 && k == gameBoard[i].length / 2 + 2) {
+                    System.out.print("MP : " + playerMP);
 
-                } else if (i == gameBoard.length - 2 && k == gameBoard[i].length / 2) {
+                } else if (i == gameBoard.length - 2 && k == gameBoard[i].length / 4) {
                     System.out.print('H');
                 } else if (k >= 0 && k < gameBoard[i].length) {
                     System.out.print(' ');
                 }
-                if (k == gameBoard[i].length - 1) {
+                if (k == gameBoard[i].length / 2) {
                     System.out.print('|');
                 }
 
             }
             System.out.println();
+        }
+    }
+
+    public void updateBoard() {
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
         }
     }
 }
