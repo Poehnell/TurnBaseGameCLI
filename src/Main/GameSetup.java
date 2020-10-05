827fdc394d1ccbcd5fd49b62f3121fba517b1454
Ty <poehnell28@gmail.com>
2020-10-04 19:19:06 -0600

    added dope custom sound track

--- src/Main/GameSetup.java
+++ src/Main/GameSetup.java
@@ -2,10 +2,18 @@ package Main;
 
 import Player.Job;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.*;
+import java.io.File;
+
+
 public class GameSetup {
 
 
     public GameSetup() {
+        playMusic("music.wav");
         new Intro();
         DataInput scan = new DataInput();
         scan.heroChoice();
@@ -18,5 +26,27 @@ public class GameSetup {
         combat.battle();
 
     }
+public void playMusic(String musicLocation){
+        try {
+            File musicPath = new File(musicLocation);
 
+            if (musicPath.exists()) {
+
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInput);
+                clip.start();
+                clip.loop(10);
+
+                JOptionPane.showMessageDialog(null, "Press OK to stop playing");
+                clip.stop();
+
+            } else {
+                System.out.println(" cant find file");
+            }
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+}
 }
